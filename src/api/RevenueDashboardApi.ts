f2caefec8da14df83a840b237dcff1c4fe6f5914
hiteshshimpi55 <shimpihitesh2@gmail.com>
2026-03-28 01:35:38 +0530

    changed api route for revenue dashboard

--- src/api/RevenueDashboardApi.ts
+++ src/api/RevenueDashboardApi.ts
@@ -2,7 +2,7 @@ import { AxiosClient } from '@/core/axios/verbs';
 import { RevenueDashboardRequest, RevenueDashboardResponse } from '@/types/dto/RevenueDashboard';
 
 class RevenueDashboardApi {
-	private static baseUrl = '/revenue-dashboard';
+	private static baseUrl = '/dashboard/revenue-dashboard';
 
 	public static async getRevenueDashboard(payload: RevenueDashboardRequest): Promise<RevenueDashboardResponse> {
 		return await AxiosClient.post<RevenueDashboardResponse>(this.baseUrl, payload);
